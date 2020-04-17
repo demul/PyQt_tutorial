26ad64e1bc733efca17c0ae318ce747da82bcde2
demul <jin4325@naver.com>
2020-04-17 13:36:20 +0900

    ...

---
+++ 3_STT_app.py
@@ -0,0 +1,6 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,\
+    QPushButton, QToolTip, QMenuBar, QLabel, QAction, QDesktopWidget,\
+    QFileDialog
+from PyQt5.QtCore import QCoreApplication, QSize
+from PyQt5.QtGui import QIcon, QFont, QPixmap
